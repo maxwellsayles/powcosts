eddd7829741c4b78796b28727d17257882ef5147
Maxwell Sayles <maxwellsayles@gmail.com>
2013-02-17 12:43:34 -0800

    updated comment

--- cost_memo_chain.h
+++ cost_memo_chain.h
@@ -1,9 +1,7 @@
 /**
  * @file cost_memo_chain.h
- * Computes optimal additive-only strictly chains partitions.
- * Expressions are of the form
- * n = 2^a1*3^b1 + 2^a2*3^b3 + ... 2^an*3^bn
- * where 2^ai*3^bi is strictly less than and divides 2^aj*3^bj for all i < j.
+ * Recursively compute the miminum cost chain, but only allowing
+ * a single term to be added or subtracted at each step.
  *
  * NOTE: This is only useful for small n (typically < 65536) since its
  * runtime is exponential in the size of the input (i.e. linear in the
