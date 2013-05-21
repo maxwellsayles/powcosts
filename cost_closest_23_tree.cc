06222b0b0de7e601b613b440b3e0f9e84772c047
Maxwell Sayles <maxwellsayles@gmail.com>
2013-05-20 17:27:36 -0700

    i think i forgot to free the representation

--- cost_closest_23_tree.cc
+++ cost_closest_23_tree.cc
@@ -27,6 +27,7 @@ double CostClosest23Tree::cost(const group_cost_t& cost,
     if (terms[i].a > max_a) max_a = terms[i].a;
     if (terms[i].b > max_b) max_b = terms[i].b;
   }
+  free(terms);
   return cost.square * max_a
          + cost.cube * max_b
          + (term_count - 1) * cost.compose;
